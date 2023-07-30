134a503ca1074da12c1e47e9bebc91ec88de0316
Dominikus Domero <mikaeldominikus@gmail.com>
2023-07-30 16:20:11 +0700

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+	host ='localhost',
+	user = 'root',
+	passwd = ''
+	)
+
+# prepare a cursor object
+cursorObject = database.cursor()
+
+# create a database
+cursorObject.execute("CREATE DATABASE elderco")
+print("ALL Done")
